56faa647b9b932e2cba7ee4af90e81b63c349233
rafaelRato <rafaelcteixeira1@hotmail.com>
2020-06-15 22:54:34 -0300

    Primeiro esboço do projeto

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-4 text-center bg-gray-400'>
+      MyDailyStatus é um projeto criado durante o Full Stack Lab do DevPleno.<br></br>
+      Implementado por:{' '}
+      <a href='https://linkedin.com/in/rafael-cteixeira'>Rafael Teixeira</a>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './NavBar'
+
+const Header = () => {
+  return (
+    <div className='bg-gray-200'>
+      <h1>
+        <img className='h-24 mx-auto py-4' 
+        src='/logo.png' 
+        alt='Olá Mundo!' 
+        height='60' />
+      </h1>
+      <NavBar />
+    </div>
+  );
+}
+
+export default Header;
